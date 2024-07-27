a2e9ce1e026b4c6aac274778d8ceb9b469a2ab9b
Yousef Zekry <yousefzekry.yz@gmail.com>
2024-07-27 20:40:02 +0300

    feat: Implement forgot password functionality
    - Modified `controllers/authController.js`:
      - Added `forgotPassword` request:
        - Receives email from user input and checks if the user exists.
        - Handles errors if the user does not exist.
        - Generates a random token as a temporary password and validates it before saving.
    
    - Modified `model/userModel.js`:
      - Required `crypto` package.
      - Created `createPasswordToken` method to generate a random token, encrypt it, and set a 10-minute expiration.
    
    - Modified `routes/userRoutes.js`:
      - Added new routes for `forgotPassword` and `resetPassword`.

--- controllers/authController.js
+++ controllers/authController.js
@@ -108,3 +108,18 @@ exports.restrictTo = (...roles) => {
 		next();
 	};
 };
+
+exports.forgotPassword = asyncWrapper(async (req, res, next) => {
+	//1) Get user based on POSTED email
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) {
+		return next(new errorHandler("There is no user with email address.", 404));
+	}
+
+	//2) Generate the random token
+	const resetToken = user.createPasswordResetToken();
+	await user.save({ validateBeforeSave: false });
+	//3) Send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,9 +8,16 @@ const {
 	updateUser,
 	deleteUser,
 } = require("../controllers/userController");
-const { signup, login } = require("./../controllers/authController");
+const {
+	signup,
+	login,
+	forgotPassword,
+	resetPassword,
+} = require("./../controllers/authController");
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/forgotPassword", forgotPassword);
+router.post("/resetPassword", resetPassword);
 
 router.route("/").get(getAllUsers).post(createUser);
 
